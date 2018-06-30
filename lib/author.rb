86526777cc58b6a6c923a7de9553028be3283961
Karen Flores <karen.ag.flores@gmail.com>
2018-06-30 08:54:37 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -8,10 +8,12 @@ class Author
 
   def add_post(post)
     @posts << post
+    post.author = self
   end
 
   def add_post_by_title(title)
-    self.post.new(title)
+    post = Post.new(title)
+    self.add_post
   end
 
   def self.post_count
